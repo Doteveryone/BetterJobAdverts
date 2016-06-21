8841c6da36e1f3bdf4803506f4520084961cbd71
Richard Pope <richardpope@MacBook-Pro-2.local>
2016-06-21 10:29:57 +0100

    auth

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', None)
+    BASIC_AUTH_USERNAME = os.environ.get('BASIC_AUTH_USERNAME', None)
+    BASIC_AUTH_PASSWORD = os.environ.get('BASIC_AUTH_PASSWORD', None)
 
 class DevelopmentConfig(Config):
     API_BASE_URL = "http://localhost:5000/api"
--- jobcert/__init__.py
+++ jobcert/__init__.py
@@ -6,11 +6,13 @@ from flask.ext.api import renderers
 from flask.ext.api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask.ext.basicauth import BasicAuth
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('SETTINGS', 'config.DevelopmentConfig'))
 api = Api(app)
+basic_auth = BasicAuth(app)
 # db = SQLAlchemy(app)
 # db.create_all()
 
