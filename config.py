2886ee4bdae8e94d9d7a55f883416b1bc31c91f7
Richard Pope <richardpope@MacBook-Pro-2.local>
2016-06-21 10:37:05 +0100

    basic auth

--- config.py
+++ config.py
@@ -5,12 +5,15 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', None)
     BASIC_AUTH_USERNAME = os.environ.get('BASIC_AUTH_USERNAME', None)
     BASIC_AUTH_PASSWORD = os.environ.get('BASIC_AUTH_PASSWORD', None)
+    BASIC_AUTH_FORCE = True
 
 class DevelopmentConfig(Config):
     API_BASE_URL = "http://localhost:5000/api"
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
     SECRET_KEY = 'not-a-secret'
+    BASIC_AUTH_FORCE = False
+
 
 class TestConfig(DevelopmentConfig):
     TESTING = True
